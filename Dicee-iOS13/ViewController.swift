af3b0e9322ac4808ef6f65ad7d5b3ba51d603957
Raees-Ahamed <49068534+Raees-Ahamed@users.noreply.github.com>
2020-07-21 12:19:23 -0700

    updated dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1:
+    UIImageView!
+
+    @IBOutlet weak var diceImageView2:
+    UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+        
+        //in order to change the opacity of the button we can type the below code
+        //diceImageView1.alpha = 0.5
+        
+        //diceImageView2.alpha = 0.5
     }
-
-
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        if(leftDiceNumber>5){
+            leftDiceNumber = 0
+        }
+        else{
+            diceImageView2.image =
+            [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+             rightDiceNumber = leftDiceNumber - 1
+        }
+        if(rightDiceNumber < 0){
+            leftDiceNumber = 5
+        }
+        else{
+            diceImageView1.image =
+                       [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+            leftDiceNumber = leftDiceNumber + 1
+        }
+    }
+    
 }
 
